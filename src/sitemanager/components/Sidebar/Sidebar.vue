494ef85012c34c17a3567ccf4ed8340743981239
Diluksha Nimshan Munasinghe <167074999+ThinethND@users.noreply.github.com>
2025-10-24 14:16:12 +0530

    just a commit do not take

--- src/sitemanager/components/Sidebar/Sidebar.vue
+++ src/sitemanager/components/Sidebar/Sidebar.vue
@@ -18,7 +18,7 @@
         class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
         to="/"
       >
-        Vue Notus
+        Site Manager Control
       </router-link>
       <!-- User -->
       <ul class="md:hidden items-center flex flex-wrap list-none">
@@ -44,7 +44,7 @@
                 class="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
                 to="/"
               >
-                Vue Notus
+                Site Manager Control 
               </router-link>
             </div>
             <div class="w-6/12 flex justify-end">
@@ -99,7 +99,7 @@
                   class="fas fa-tv mr-2 text-sm"
                   :class="[isActive ? 'opacity-75' : 'text-blueGray-300']"
                 ></i>
-                Dashboard sitemanager
+                Dashboard
               </a>
             </router-link>
           </li>
