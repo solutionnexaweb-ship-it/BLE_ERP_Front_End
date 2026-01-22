69cbf7b8204b9291715c14e2a38265b3be8768e4
olitha duvin <olithasamaraweera2002@gmail.com>
2026-01-22 22:20:12 +0530

    connect with uat

--- vue.config.js
+++ vue.config.js
@@ -5,10 +5,12 @@ module.exports = {
     port: 8081,
     proxy: {
       'ble/api': {
-        target: "http://localhost:8080",
+        target: "http://13.212.91.58:8080",
+        // target: "http://localhost:8080",
         changeOrigin: true,
         secure: false
       }
     }
   }
 };
+
